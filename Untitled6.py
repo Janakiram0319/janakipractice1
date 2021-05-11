28b670dff7b4827b86c60f2935d1d146362b8aa3
Janakiram0319 <83774097+Janakiram0319@users.noreply.github.com>
2021-05-11 07:35:12 +0530

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+movies=['spiderman','thor','captain america','iron man']
+
+
+# In[2]:
+
+
+print(movies)
+
+
+# In[5]:
+
+
+movies.append('wonder women')
+
+
+# In[6]:
+
+
+print(movies)
+
+
+# In[8]:
+
+
+movies.insert(2,'godzilla')
+
+
+# In[9]:
+
+
+print(movies)
+
+
+# In[10]:
+
+
+movies[1]='kong'
+
+
+# In[11]:
+
+
+print(movies)
+
+
+# In[12]:
+
+
+movies.pop()
+
+
+# In[13]:
+
+
+print(movies)
+
+
+# In[15]:
+
+
+movies.sort()
+
+
+# In[16]:
+
+
+print(movies)
+
+
+# In[17]:
+
+
+movies.reverse()
+
+
+# In[18]:
+
+
+print(movies)
+
+
+# In[19]:
+
+
+len(movies)
+
+
+# In[20]:
+
+
+my_hobbies=['watching t.v','watching movies','playing cricket']
+
+
+# In[21]:
+
+
+print(my_hobbies)
+
+
+# In[22]:
+
+
+myfriend_hobbies=my_hobbies
+
+
+# In[23]:
+
+
+print(myfriend_hobbies)
+
+
+# In[24]:
+
+
+characters=['rocket','root','spark','chris evans']
+
+
+# In[25]:
+
+
+print(characters)
+
+
+# In[26]:
+
+
+for character in characters:
+    print(character)
+
+
+# In[30]:
+
+
+for g in characters:
+    print(g)
+
+
+# In[31]:
+
+
+print('here is the list of the my favourite characters')
+for f in characters:
+    print(f)
+
+
+# In[32]:
+
+
+print(characters)
+
+
+# In[33]:
+
+
+print(characters[1])
+
+
+# In[34]:
+
+
+print(characters[3])
+
+
+# In[36]:
+
+
+print(characters[-1])
+
+
+# In[37]:
+
+
+print(characters)
+
+
+# In[38]:
+
+
+len(characters)
+
+
+# In[39]:
+
+
+print(characters[1:3])
+
+
+# In[41]:
+
+
+print(characters[2:4])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
